adde3438ab3d1ec511e4a174cc097ab520ea62f8
dani636 <daniel.schukies@gmail.com>
2013-01-25 09:00:45 +0100

    logik fix

--- Or.java
+++ Or.java
@@ -9,7 +9,7 @@ public class Or extends Logikgatter
 		
 	/** @return boolean gibt true zurueck, wenn der vorgegebene Ausgang erreicht wurde */
 	public boolean pruefeAusgang(boolean eingangszustand1, boolean eingangszustand2){
-		if ( eingangszustand1 & eingangszustand2 != super.getAusgang() ){
+		if ( (eingangszustand1 | eingangszustand2) != super.getAusgang() ){
 			return false;
 		} else
 		{
