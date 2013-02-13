78362d35dc00bf06654ec39dda07daefcd101029
dani636 <daniel.schukies@gmail.com>
2013-02-13 15:30:47 +0100

    KI Methode erweitert

--- Spieler.java
+++ Spieler.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class Spieler 
 {
@@ -187,7 +189,80 @@ public class Spieler
 		}
 		else // Anderes Gatter muss gespielt werden.
 		{
+			/** 
+			 * Indizes der ersten Dimension des Arrays stehen fuer die Gatter und der Inhalt dieser Indizes steht fuer die Prioritaet
+			 * In der zweiten Dimsension wird Reihe,Index und Prioritaet des Ablegeplatzes gespeichert.
+			 *   0       1        2
+			 * [reihe][index][prioritaet] x4
+			 *  */
+			int[][] gatterPrioritaet = new int[4][3];
 			
+			/** 
+			 * 	Durchlaeuft das Spielfeld und schreibt in das Array Reihe, Index und die Anzahl der moeglichen nachfolgenden Logikgatter, 
+			 *  welche legbar sind. 
+			 *  */
+			for(int reihe = 0; reihe < 4; reihe++ )
+			{
+				gatterPrioritaet[reihe][2] = 0 ; // Setze Prioritaet auf 0
+				
+				for(int index = 0; index < 4; index++) // ungueltige Indizes werden abgefangen.
+				{
+					if( !(this.logikgatter[reihe] instanceof Not) ) // pruefe, ob Spielergatter kein Not ist.
+					{
+						/** Lege Logikgatter auf Spielfeld, wenn es gesetzt werden kann und pruefe, ob weitere Gatter gelegt werden koennen */
+						if( kopieEigenesSpielfeld.setLogikgatter(reihe, index, this.getLogikgatter(reihe), kopieBitfolge) )
+						{
+							gatterPrioritaet[reihe][2]++; // erhoehe Prioritaet.
+							
+							for(int i = 0; i < 4; i++)
+							{
+								/** Versucht weitere Gatter zu legen, um die Prioritaet des gelegten Gatters zu bestimmen */
+								for(int probeReihe = 0; probeReihe < 4; probeReihe++ )
+								{
+									for(int probeIndex = 0; probeIndex < 4; probeIndex++) // ungueltige Indizes werden abgefangen.
+									{
+										/** pruefe, ob das Probegatter nicht Not ist und nicht mit dem testweise gelegtem Gatter uebereinstimmt */
+										if( !(this.logikgatter[probeReihe] instanceof Not && probeReihe == reihe) )
+										{
+											if( kopieEigenesSpielfeld.setLogikgatter(probeReihe, probeIndex, this.getLogikgatter(probeReihe), kopieBitfolge) )
+											{
+												gatterPrioritaet[reihe][0] = reihe;
+												gatterPrioritaet[reihe][1] = index;
+												gatterPrioritaet[reihe][2] += 1 ;
+											}
+										}
+									}
+								}
+							}
+							
+							kopieEigenesSpielfeld = (Spielfeld)eigenesSpielfeld.clone(); // Setze kopie des Spielfelds zurueck.
+						}
+					}
+				}
+			}
+			
+			
+			/** Sucht Gatter mit hoechster prioritaet */
+			int priorisiertesGatter = 0;
+			
+			for(int i = 0; i < 4;i++)
+			{
+				if(gatterPrioritaet[i][2] > gatterPrioritaet[priorisiertesGatter][2] )
+				{
+					priorisiertesGatter = i;
+				}
+			}
+			
+			/** Entscheide, ob Logikgatter auf Spielfeld gelegt werden soll oder ein Neues gezogen werden soll */
+			if(gatterPrioritaet[priorisiertesGatter][2] > 0)
+			{
+				eigenesSpielfeld.setLogikgatter(gatterPrioritaet[priorisiertesGatter][0], gatterPrioritaet[priorisiertesGatter][1], this.logikgatter[priorisiertesGatter], bitfolge);
+				this.zieheNeuesLogikgatter(priorisiertesGatter);
+			}
+			else // Ziehe Neues
+			{
+				this.zieheNeuesLogikgatter(new Random().nextInt(4)); 
+			}
 		}
 
 		
