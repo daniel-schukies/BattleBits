2162bf7a770322d4cc1c4a0d2eb5629856091e4a
Linux <louismusssterben@googlemail.de>
2013-05-26 19:58:49 +0200

    alten Code entfernt

--- GUI/Game/SoundAusgabe.java
+++ GUI/Game/SoundAusgabe.java
@@ -21,7 +21,7 @@ public class SoundAusgabe {
 	private AudioFormat format;
 	private  DataLine.Info info;
 	private  Clip clip;
-	private URL  audioFileURL;;
+	private URL  audioFileURL;
 	private List<AudioClip> audioliste = new ArrayList<AudioClip>();
 	
 	public SoundAusgabe()
@@ -50,15 +50,7 @@ public class SoundAusgabe {
 	
 	public void playWarning()
 	{
-		/*  InputStream path = getClass().getResourceAsStream("/warning.wav");
-		  	try{
-		  	    clip = AudioSystem.getClip();
-		  	    clip.open(AudioSystem.getAudioInputStream(path));
-		  	    clip.start();
-		  	}catch(Exception e)
-		  	{
-		  		System.out.println("Fehler");
-		  	}*/
+
 	  	 try { 
 	  		 this.audioFileURL = getClass().getResource("/warning.wav");
 	  		 System.out.println("Playing: " + audioFileURL); 
@@ -76,43 +68,17 @@ public class SoundAusgabe {
 	  		 DataLine.Info info = new DataLine.Info(Clip.class, af, size);
 	  		 audioInputStream.read(audio, 0, size);
 	  		 Clip clip = (Clip) AudioSystem.getLine(info);
-	  		 clip.open(af, audio, 0, size); clip.start(); // Das musste ich auskommentieren, sonst spielt er bei mir gar nichts. // while (clip.isActive()) { // } // clip.stop(); } catch (Exception e) { e.printStackTrace(); .. mehr auf http://w-w-w.ms/a33t56
-	  	 }catch(Exception e)
+	  		 clip.open(af, audio, 0, size); clip.start(); 
+	  	}catch(Exception e)
 	  	 {
 	  		 System.out.println("Fail");
 	  	 }
-		/*try
-		{
-			URL url = getClass().getResource("/warning.wav");
-			this.stream = AudioSystem.getAudioInputStream(url);
-		    this.format = stream.getFormat();
-		    this.info = new DataLine.Info(Clip.class, format);
-		    this.clip = (Clip) AudioSystem.getLine(info);
-		    this.clip.open(stream);
-		    this.clip.start();
-		
-		}catch(Exception e)
-		{
-			System.out.println("Sound Fehler");
-		}*/
-		
-		//URL sound_url = getClass().getResource("/warning.wav");
-        //AudioClip audio = (AudioClip)Applet.newAudioClip(sound_url);
-      //  audioliste.add(audio);
-       // audio.play();
+
 	}
 	
 	public void playError()
 	{
-	/*	  InputStream path = getClass().getResourceAsStream("/error.wav");
-		  	try{
-		  	    clip = AudioSystem.getClip();
-		  	    clip.open(AudioSystem.getAudioInputStream(path));
-		  	    clip.start();
-		  	}catch(Exception e)
-		  	{
-		  		System.out.println("Fehler");
-		  	}*/
+
 	  	 try { 
 	  		 this.audioFileURL = getClass().getResource("/error.wav");
 	  		 System.out.println("Playing: " + audioFileURL); 
@@ -130,45 +96,16 @@ public class SoundAusgabe {
 	  		 DataLine.Info info = new DataLine.Info(Clip.class, af, size);
 	  		 audioInputStream.read(audio, 0, size);
 	  		 Clip clip = (Clip) AudioSystem.getLine(info);
-	  		 clip.open(af, audio, 0, size); clip.start(); // Das musste ich auskommentieren, sonst spielt er bei mir gar nichts. // while (clip.isActive()) { // } // clip.stop(); } catch (Exception e) { e.printStackTrace(); .. mehr auf http://w-w-w.ms/a33t56
-	  	 }catch(Exception e)
+	  		 clip.open(af, audio, 0, size); clip.start(); 
+	  	}catch(Exception e)
 	  	 {
 	  		 System.out.println("Fail");
 	  	 }
-	/*	try
-		{
-			URL url = getClass().getResource("/error.wav");
-			this.stream = AudioSystem.getAudioInputStream(url);
-		    this.format = stream.getFormat();
-		    this.info = new DataLine.Info(Clip.class, format);
-		    this.clip = (Clip) AudioSystem.getLine(info);
-		    this.clip.open(stream);
-		    this.clip.start();
-		
-		}catch(Exception e)
-		{
-			System.out.println("Sound Fehler");
-		}*/
-	//	URL sound_url = getClass().getResource("/error.wav");
-     //   AudioClip audio = (AudioClip)Applet.newAudioClip(sound_url);
-      //  audioliste.add(audio);
-     //   audio.play();
+
 	}
 	
 	public void playNeuZiehen()
 	{
-		/*  InputStream path = getClass().getResource("/neuziehen.wav");
-		  	try{
-		  	    clip = AudioSystem.getClip();
-		  	    clip.open(AudioSystem.getAudioInputStream(path));
-		  	    clip.start();
-		  	}catch(Exception e)
-		  	{
-		  		System.out.println("Fehler");
-		  	}
-		  	
-		  	InputStream is= getClass().getResourceAsStream("/images/ads/WindowsNavigationStart.wav");
-		  	AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(is);*/
 		  	
 		  	 try { 
 		  		 this.audioFileURL = getClass().getResource("/neuziehen.wav");
@@ -187,32 +124,12 @@ public class SoundAusgabe {
 		  		 DataLine.Info info = new DataLine.Info(Clip.class, af, size);
 		  		 audioInputStream.read(audio, 0, size);
 		  		 Clip clip = (Clip) AudioSystem.getLine(info);
-		  		 clip.open(af, audio, 0, size); clip.start(); // Das musste ich auskommentieren, sonst spielt er bei mir gar nichts. // while (clip.isActive()) { // } // clip.stop(); } catch (Exception e) { e.printStackTrace(); .. mehr auf http://w-w-w.ms/a33t56
-		  	 }catch(Exception e)
+		  		 clip.open(af, audio, 0, size); clip.start();
+				}catch(Exception e)
 		  	 {
 		  		 System.out.println("Fail");
 		  	 }
 		  	
-		/*try
-		{
-			URL url = getClass().getResource("/neuziehen.wav");
-			this.stream = AudioSystem.getAudioInputStream(url);
-		    this.format = stream.getFormat();
-		    this.info = new DataLine.Info(Clip.class, format);
-		    this.clip = (Clip) AudioSystem.getLine(info);
-		    this.clip.open(stream);
-		    this.clip.start();
-		
-		}catch(Exception e)
-		{
-			System.out.println("Sound Fehler");
-		}*/
-		
-	//	URL sound_url = getClass().getResource("/neuziehen.wav");
-    //    AudioClip audio = (AudioClip)Applet.newAudioClip(sound_url);
-        //audioliste.add(audio);
-    //    audio.play();
-		
 	}
 	
 }
