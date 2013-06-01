a50212a5ede6bb3d908711679ae98e849c2fe98d
DANI <daniel.schukies@gmail.com>
2013-06-01 19:58:57 +0200

    Konsolen Modus

---
+++ Main.java
@@ -0,0 +1,39 @@
+import GUI.Game.GameFrame;
+import Logik.Spiel;
+
+public class Main {
+
+	/**
+	 * @param args Konsolen-Modus
+	 */
+	public static void main(String[] args) 
+	{
+		if(args.length > 0)
+		{
+			/**
+			 * Preufe, ob Konsolenversion genutzt werden soll.
+			 */
+			if(args[0].equals("--console"))
+			{
+				/**
+				 * Starte Konsolenversion.
+				 */
+				new Spiel(false);
+			}
+			else
+			{
+				/**
+				 * Starte GUI Spiel
+				 */
+				new GameFrame();
+			}
+		}
+		else
+		{
+			/**
+			 * Starte GUI Spiel
+			 */
+			new GameFrame();
+		}
+	}
+}
